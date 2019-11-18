e1ca62b9a85ade25aa4657faf297b52cbd7d5a9e
Shane Boles <shanebboles@gmail.com>
2019-11-17 19:19:58 -0500

    Created a file showing analogue to raw webgl custom shaders

--- testScripts/testCustomShaders.js
+++ testScripts/testCustomShaders.js
@@ -1,10 +1,10 @@
-//just puts a box in a scene, and spins it (!!)
+//Exploration into using custom shaders instead of the Three.js materials
 
 //import THREE into the file
 // work on this? right now I'm just pulling into namespace with a script tag
 
 //create scene
-var scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
 //perspective camera (fov, aspect ratio, near clip, far clip)
 var camera = new THREE.PerspectiveCamera(75, 1600/900, 0.1, 1000);
@@ -14,29 +14,40 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(1600, 900);
 document.body.appendChild(renderer.domElement);
 
-//This is a bit different, so stick with me
-//we create a geometry and material object (box of sides 1,1,1, flat white color)
+//create a geometry (box of sides 1,1,1)
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial( { color: 0xCCCCCC } );
 
-//...and then we apply the mesh object to the geometry object to get a cube, then
-// add it to the scene.
+//let's see an example of creating a custom material, instead (material == shader)
+//var material = new THREE.MeshBasicMaterial( { color: 0xCCCCCC } );
+var material = new THREE.ShaderMaterial({
+
+	//uniforms must be declared in both this block AND the vertexShader
+	uniforms:{
+
+	},
+
+	//we can just pull from the DOM for shaders, though we need to get strings (?)
+	//also, ShaderMaterial prepends some pre-declared stuff for convenience.
+	//RawShaderMaterial does not
+	vertexShader: document.getElementById('vertex-shader').textContent,
+
+	fragmentShader: document.getElementById('fragment-shader').textContent
+});
+
+//the rest is the same. Apply material to geometry, etc
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 6;
-camera.position.x = 3;
-camera.position.y = 2;
+camera.position.z = 2;
+camera.position.x = 0;
+camera.position.y = 0;
 
-//typical requestAnimFrame loop...
+
 function render(){
 	requestAnimationFrame(render);
 
-	//..except this. We tell the renderer which scene, and with which camera,
-	//to render.
 	renderer.render(scene, camera);
 
-	//rotation is abstracted away, or at least by default
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 }
