66b5754450354d36ec88e3c74c1239cbff6e0201
Prasantacharya <setinadi@gmail.com>
2019-11-19 21:48:19 -0500

    Added a hashtable so it would be easier to change the rules of the l-system

--- strReplace.js
+++ strReplace.js
@@ -7,18 +7,22 @@ String replacement method:
 Replace "A" with "BA"
 Replace "B" with "A"
 */
+var rules = {
+"A":"AB",
+"B":"A"
+};
+// rules for special characters
+
 var iterations = 5;
 var replace = "";
 for(i = 0; i < iterations; i++){
 	for each(var character in string){
-		if(character === "A"){
-			replace += "AB"
-		}else if(character === "B"){
-			replace += "A"
-		}else{
+		if(character in rules){ // do the replacement if it is special
+			replace += rules[character]
+		}else{ // if the character is not special, then don't do anything
 			replace += character
 		}
 	}
 	string = replace;
-	console.log(string);
+	// console.log(string); // just to verify if it works
 }
